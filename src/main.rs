a55105985093e62466d99af63788e2529733c2c3
BlackCatCmx <146694822+BlackCatCmx@users.noreply.github.com>
2026-04-09 16:28:00 +0800

    Use jemalloc on Linux

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(target_os = "linux")]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     token_refresh::cli::run().await
